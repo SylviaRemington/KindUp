3dadb40403bc9de9b5b4cc6aef9a9adf16d46759
SylviaRemington <sylviaremington@gmail.com>
2025-06-24 15:41:16 -0700

    testing to make sure localhost connection is working - successful

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 // KindUp App - creating random acts of kindness & upleveling the world
 
-
+ 
+// -----------------------------IMPORTS----------------------------------------------------
 
 const express = require('express');
-const app = express();
-const port = 3000
+const app = express(); // creating the app using express so can build out routes, handle requests & send responses
+const port = 3000;
+
+
+// -----------------------------ROUTES-----------------------------------------------------
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello, all you supahstaaars out there!')
+});
+
+
+
+
+// ----------Starts the app and tells it to listen for requests on PORT (3000)-------------
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
