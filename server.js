87ec67b1d97b195aa27ac9a53133bdb71be40f7a
SylviaRemington <sylviaremington@gmail.com>
2025-06-30 16:08:24 -0700

    creating dynamic route for showpage

--- server.js
+++ server.js
@@ -86,9 +86,10 @@ app.get('/kindacts/new', async (req, res) => { //this is the url route
 });
 
 // SHOWPAGE ROUTE - to make links from index-of-kind-acts.ejs dynamic
-app.get('/kindacts/:kindactId', (req, res) => {
-  
-  res.send('Showpage');
+app.get('/kindacts/:kindactId', async (req, res) => {
+  const foundKindAct = await KindAct.findById(req.params.kindactId);
+  // res.send(`This route renders the showpage for title: ${req.params.kindactId}.`);
+  res.send(`This route renders the showpage for the Kind Act named: ${foundKindAct.title}.`);
 });
 
 // POST ROUTE - for form on new.ejs
