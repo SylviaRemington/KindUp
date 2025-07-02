8d78348720474d98af1bca90f9028fb1b52e7b75
SylviaRemington <sylviaremington@gmail.com>
2025-07-02 15:47:33 -0700

    adding to auth.js file in controller with require and router info and exporting router

---
+++ controllers/auth.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router(); 
+//Router creates an object where we can define all of our end points & get methods.
+//Will be moving a lot of the app.gets into the Router and out of the server.js
+//Router is a piece of middleware.
+
+
+
+module.exports = router; //exporting router from here
