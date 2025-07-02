129e3c66184f20d695d09ae8a33842fd204812ca
SylviaRemington <sylviaremington@gmail.com>
2025-07-02 15:06:38 -0700

    User.js model created

--- models/User.js
+++ models/User.js
@@ -1,2 +1,20 @@
 // Need to put users into the database - So need to create model for it
 
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
+
+
