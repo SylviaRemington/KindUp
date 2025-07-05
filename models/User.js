45d7ad96f75b72074dc78c6ed02116af8cc4d075
SylviaRemington <sylviaremington@gmail.com>
2025-07-05 13:07:30 -0700

    notes on relationship between User.js model and mongoose relationships and creating a todo

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 // Need to put users into the database - So need to create model for it
 // This User.js model was created during auth section & correlates with that.
+// Need to have a User first before you have a Todo for the user.
 
 const mongoose = require('mongoose');
 
@@ -8,6 +9,8 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     // unique: true, // adding unique with function in code at auth.js line 30
+    // There will be a unique id created for this user... And need this user id 
+    // in order to create a mongoose relationship & a todo.
   },
   password: {
     type: String,
