acb6838d593b17fa0cd81e07b716e4ceb266a4e2
SylviaRemington <sylviaremington@gmail.com>
2025-07-05 15:48:10 -0700

    notes as to what I still need to do for models comment.js section & controllers comment-actions.js section

--- controllers/comment-actions.js
+++ controllers/comment-actions.js
@@ -1,3 +1,5 @@
 // This controller file handles the comment actions (create, update, delete functionality).
 // Handling routes and functions
 
+// This will contain my routes and the logic (e.g. what happens when a comment is created or deleted).
+// ! Make sure to require and run this file of comment-actions.js in my server.js file
--- models/Comment.js
+++ models/Comment.js
@@ -1,1 +1,4 @@
 // Comment Schema Model
+// This will define what a comment is for my app - e.g. the comment content &
+// & a reference to the user who wrote it.
+
