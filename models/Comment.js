15395d2550f8886be856ee90c97014fc2c4056a3
SylviaRemington <sylviaremington@gmail.com>
2025-07-05 15:57:10 -0700

    information of what is needed for comment model so that user will be linked to their comment

--- models/Comment.js
+++ models/Comment.js
@@ -2,3 +2,14 @@
 // This will define what a comment is for my app - e.g. the comment content &
 // & a reference to the user who wrote it.
 
+/*
+INFORMATION I NEED TO ADD IN MODEL SO THAT EACH COMMENT IS LINKED TO THE USER WHO WROTE IT:
+
+General info from chatgpt --
+What do I need to create to make sure each comment knows who wrote it? Don't give me code.
+You don’t need a new file.
+✅ In models/Comment.js, add a reference to the User (the one who wrote the comment). // so double check syntax from lecture on Mongoose Relationships & Skyrockit.
+✅ In controllers/comment-actions.js, when someone submits a comment, include the logged-in user's ID in the data you save.
+
+*/
+
