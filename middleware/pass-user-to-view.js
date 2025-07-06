f854e70d4c3166ef9d9bd03d8eb35a08c478d9a5
SylviaRemington <sylviaremington@gmail.com>
2025-07-06 13:49:23 -0700

    notes on middleware is-signed-in and pass-user-to-view sections

---
+++ middleware/pass-user-to-view.js
@@ -0,0 +1,3 @@
+// I don't know if I actually need this page since I already have sign-in & sign-out functionality under views in the auth folder
+// If I need to add this section, this info can be found at Skyrockit Lecture Part One at 43:18.
+// But keeping this notation in for reference and future awareness... when use this as a reference for future apps.
