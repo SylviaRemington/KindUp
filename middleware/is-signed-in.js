410ff8b050e728a3b16b368249a4d87435c4de5a
SylviaRemington <sylviaremington@gmail.com>
2025-07-06 13:14:46 -0700

    notation of where to find lecture information regarding is-signed-in and pass-user-to-view

--- middleware/is-signed-in.js
+++ middleware/is-signed-in.js
@@ -1,2 +1,3 @@
 // I don't know if I actually need this page since I already have 
 // sign-in & sign-out functionality under views in the auth folder
+// If I need to add this section, this info can be found at Skyrockit Lecture Part One at 43:18.
