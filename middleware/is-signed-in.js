92d7919f2c90aacae90889cfdbe02970e1c65c2d
SylviaRemington <sylviaremington@gmail.com>
2025-07-06 13:30:27 -0700

    addtl notes of how this middleware is another option instead of auth

--- middleware/is-signed-in.js
+++ middleware/is-signed-in.js
@@ -1,3 +1,8 @@
 // I don't know if I actually need this page since I already have 
 // sign-in & sign-out functionality under views in the auth folder
 // If I need to add this section, this info can be found at Skyrockit Lecture Part One at 43:18.
+
+
+// This part takes care of your authentication if you don't already have it set up.
+// Since I already have auth set up, I actually don't need this. 
+// But keeping this notation in for reference and future awareness.
